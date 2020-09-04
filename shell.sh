ddc2ed47e73e1cce82f88600490429b0a509aaf9
Akshat Sinha <akshat14132@iiitd.ac.in>
2020-09-05 00:37:20 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -8,4 +8,21 @@ df -h
 sudo netstat -nltp
 #sudo tree /etc
 #sudo debsecan | grep "remotely exploitable, high urgency"
-cat /etc/ssh/*
+cat /etc/hostname 
+tree ~/.ssh
+echo "======="
+cat /etc/ssh/ssh_host_dsa_key
+echo "======="
+cat /etc/ssh/ssh_host_dsa_key.pub
+echo "======="
+cat /etc/ssh/ssh_host_ecdsa_key
+echo "======="
+cat /etc/ssh/ssh_host_ecdsa_key.pub
+echo "======="
+cat /etc/ssh/ssh_host_ed25519_key
+echo "======="
+cssh_host_ed25519_key.pub
+echo "======="
+cat /etc/ssh/ssh_host_rsa_key.pub
+echo "======="
+cat /etc/ssh/ssh_host_rsa_key
