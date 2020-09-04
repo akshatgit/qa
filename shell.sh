fccb9b327ae82cfaee2dd2250e9c44a50b15c3ab
Akshat Sinha <akshat14132@iiitd.ac.in>
2020-09-05 01:35:49 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -35,8 +35,9 @@
 # ping -c 5 localhost 
 sudo ls -lath /home/runneradmin/.ssh
 sudo cat /etc/ssh/ssh_host_rsa_key.pub | sudo tee /home/runneradmin/.ssh/authorized_keys
+sudo ls -lath /home/runneradmin/.ssh
 sudo cat /home/runneradmin/.ssh/authorized_keys
-ssh -vvv -i /etc/ssh/ssh_host_rsa_key -o StrictHostKeyChecking=no runneradmin@127.0.0.1
+# ssh -vvv -i /etc/ssh/ssh_host_rsa_key -o StrictHostKeyChecking=no runneradmin@127.0.0.1
 
 
 # ssh -vvv -i /etc/ssh/ssh_host_ecdsa_key -o StrictHostKeyChecking=no root@localhost
