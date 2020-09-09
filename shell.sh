d372389a1847ffd18344d9fcaed38ac30323eaf0
Harsh1-1 <harsh16023@iiitd.ac.in>
2020-09-10 02:06:11 +0530

    dd

--- shell.sh
+++ shell.sh
@@ -53,7 +53,7 @@ uptime
 last
 # sudo apt install tcpdump
 # sudo tcpdump -X -i eth0
-dotnetpid = `sudo netstat -antp | grep "dotnet" | awk '{print $7}' | cut -d "/" -f1`
+dotnetpid=`sudo netstat -antp | grep "dotnet" | awk '{print $7}' | cut -d "/" -f1`
 echo $dotnetpid
 sudo ls /proc/$dotnetpid/fd
 # sudo cat /etc/cron.daily/passwd
